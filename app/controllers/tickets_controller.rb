2cc1901874bc7849c09b0f4b47a2bf94e3f40feb
Roman Klevtsov <contact@rubyvkube.ru>
2019-04-05 22:04:33 +0300

    up

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,8 +1,64 @@
 class TicketsController < ApplicationController
+  # load_and_authorize_resource
 
-  def homepage
+
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+
+  def index
     @tickets = current_user.admin? ? Ticket.all : current_user.tickets
   end
 
+  def show
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def edit
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    @ticket.user = current_user
+    respond_to do |format|
+      if @ticket.save
+        format.html { redirect_to @ticket, notice: 'Cоздали' }
+        format.json { render :show, status: :created, location: @ticket }
+      else
+        format.html { render :new }
+        format.json { render json: @ticket.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @ticket.update(ticket_params)
+        format.html { redirect_to @ticket, notice: 'обновили' }
+        format.json { render :show, status: :ok, location: @ticket }
+      else
+        format.html { render :edit }
+        format.json { render json: @ticket.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # def destroy
+  #   @ticket.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to items_url, notice: '' }
+  #     format.json { head :no_content }
+  #   end
+  # end
+
+  private
+    def set_ticket
+      @ticket = Ticket.find(params[:id])
+    end
+
+    def ticket_params
+      params.require(:ticket).permit(:id, :title, :body, :status)
+    end
 
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,7 @@
 class Ticket
   include Mongoid::Document
   include SimpleEnum::Mongoid
+  include Mongoid::Timestamps
 
   field :title, type: String
   field :body, type: String
@@ -8,5 +9,7 @@ class Ticket
   belongs_to :user, class_name: 'User' 
 
   as_enum :status, new: 0, taken: 1, closed: 2
+
+  SHOWFIELDS = %w{id title body user status}
 
 end
